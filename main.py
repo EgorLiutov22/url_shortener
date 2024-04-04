cd29f034977b79fd4cacc1fdd5ee49c1fe063b83
wayne <eliutov22@gmail.com>
2024-04-04 18:33:44 +0500

    ADD queries

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import datetime
+import random
+import string
 
 from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from wtforms import SubmitField, StringField
+from wtforms.validators import DataRequired, URL
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '12345'
@@ -9,6 +13,15 @@ app.config['SECRET_KEY'] = '12345'
 SHORT_LEN = 6
 
 db = SQLAlchemy(app)
+
+def get_short():
+        short = ''.join(random.choices(string.ascii_letters + string.ascii_letters, k=SHORT_LEN))
+        if URLmodel.query.filter(URLmodel.short == short).first():
+            return get_short()
+        else:
+            return short
+
+
 
 class URLmodel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,21 +31,40 @@ class URLmodel(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.datetime.now)
 
 
-
-
 with app.context():
     db.create_all()
 
 
+class URLForm(FlaskForm):
+    original_url = StringField('Вставьте ссылку',
+                               validators=[DataRequired(message='Ссылка не может быть пустой'),
+                                           URL(message='Неверная ссылка')])
+    submit = SubmitField('Получить короткую ссылку')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    form = URLForm()
+    if form.validate_on_submit():
+        url = URLmodel()
+        url.original_url = form.original_url.data
+        url.short = get_short()
+        db.sessin.add(url)
+        db.session.commit()
     return render_template('index.html')
+
 
 @app.route('/urls', methods=['GET'])
 def urls():
-    return render_template('urls.html')
+    urls = db.query.all()
+    return render_template('urls.html', urls=urls)
+
 
 @app.route('/<string:short>', methods=['GET'])
 def shortener(short):
-    link = 'example.com'
-    return redirect(url_for(link))
+    link = URLmodel.query.filter(URLmodel.short == short).first()
+    if link:
+        link.visits += 1
+        db.session.add(link)
+        db.session.commit()
+        return redirect(url_for(link))
