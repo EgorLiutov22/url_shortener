1e6be5d2bd68c312fbb2ca6f70b8b963bf23ff34
wayne <eliutov22@gmail.com>
2024-04-04 17:34:45 +0500

    add methods

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template, redirect, url_for
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['CSRF_token'] = '12345'
 
+db = SQLAlchemy(app)
+
+with app.context():
+    db.create_all()
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/urls')
+@app.route('/urls', methods=['GET'])
 def urls():
     return render_template('urls.html')
 
-@app.route('/<short:str>')
+@app.route('/<string:short>', methods=['GET'])
 def shortener(short):
     link = 'example.com'
     return redirect(url_for(link))
